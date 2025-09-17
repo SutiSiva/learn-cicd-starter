caa72384ee20854d90cea06fd3466c566e01187f
Sivaneswaran Sutharsan <s.sutharsan@outlook.de>
2025-09-17 13:25:11 +0200

    Fix formatting

--- main.go
+++ main.go
@@ -1,73 +1,72 @@
 package main
 
 import (
-    "database/sql"
-    "embed"
-    "io"
-    "log"
-    "net/http"
-    "os"
+	"database/sql"
+	"embed"
+	"io"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/go-chi/chi"
-    "github.com/go-chi/cors"
-    "github.com/joho/godotenv"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 
-    "github.com/bootdotdev/learn-cicd-starter/internal/database"
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
-    _ "github.com/tursodatabase/libsql-client-go/libsql"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type apiConfig struct {
-    DB *database.Queries
+	DB *database.Queries
 }
 
 //go:embed static/*
 var staticFiles embed.FS
 
 func main() {
-    err := godotenv.Load(".env")
-    if err != nil {
-        log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
-    }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        log.Fatal("PORT environment variable is not set")
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
 
-    apiCfg := apiConfig{}
+	apiCfg := apiConfig{}
 
-    // https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
-    // libsql://[your-database].turso.io?authToken=[your-auth-token]
-    dbURL := os.Getenv("DATABASE_URL")
-    if dbURL == "" {
-        log.Println("DATABASE_URL environment variable is not set")
-        log.Println("Running without CRUD endpoints")
-    } else {
-        db, err := sql.Open("libsql", dbURL)
-        if err != nil {
-            log.Fatal(err)
-        }
-        dbQueries := database.New(db)
-        apiCfg.DB = dbQueries
-        log.Println("Connected to database!")
-    }
+	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
+	// libsql://[your-database].turso.io?authToken=[your-auth-token]
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Println("DATABASE_URL environment variable is not set")
+		log.Println("Running without CRUD endpoints")
+	} else {
+		db, err := sql.Open("libsql", dbURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbQueries := database.New(db)
+		apiCfg.DB = dbQueries
+		log.Println("Connected to database!")
+	}
 
-    router := chi.NewRouter()
+	router := chi.NewRouter()
 
-    router.Use(cors.Handler(cors.Options{
-        AllowedOrigins:   []string{"https://*", "http://*"},
-        AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-        AllowedHeaders:   []string{"*"},
-        ExposedHeaders:   []string{"Link"},
-        AllowCredentials: false,
-        MaxAge:           300,
-    }))
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 }
 
 // unused ist eine absichtlich ungenutzte Funktion zum Testen von Staticcheck
 func unused() {
-    // diese Funktion macht nichts
-    // und wird nirgendwo aufgerufen
+	// diese Funktion macht nichts
+	// und wird nirgendwo aufgerufen
 }
-
